dda2e479dd89c6085e4757a73bedae3d389aef62
dominikglandorf <do.gl@gmx.de>
2019-03-15 18:51:40 +0100

    alter sub

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,34 @@
 const express = require('express');
-const config = require('../config.js');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 const router = express.Router();
+const config = require('../config.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Edunym' });
 });
 
 /* POST LTI Message */
-router.post('/outgoing', function(req, res, next) {
-  if (!req.query.tool_url) throw new Error('Request query has no client_url.')
-  if(!req.body.id_token) throw new Error('Request body has no id_token')
+router.post('/outgoing', (req, res) => {
+  if (!req.query.tool_url) throw new Error('Request query has no client_url.');
+  if (!req.body.id_token) throw new Error('Request body has no id_token');
 
   // TODO: create database
   // database: client public key, pseudonyms
 
+  const idToken = jwt.decode(req.body.id_token);
   // TODO: verify message
 
   // TODO: replace pseudonym
+  idToken.sub = 'you have been pseudonymized';
 
   // TODO: replace platform urls to edunym urls
 
   res.render('lti', {
     url: decodeURI(req.query.tool_url),
-    idToken: req.body.id_token,
+    idToken: jwt.sign(idToken, config.platform.privateKey, { algorithm: 'RS256' }),
   });
 });
 
