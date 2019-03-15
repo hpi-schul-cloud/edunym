a88618fb447ac08781a5903b700ef71aaa9ac8fc
dominikglandorf <do.gl@gmx.de>
2019-03-15 13:06:25 +0100

    add lti view

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,20 @@ router.get('/', function(req, res, next) {
 
 /* POST LTI Message */
 router.post('/', function(req, res, next) {
-  // TODO: create configuration, database and
-  // configuration: platform private key, platform public key
+  // TODO: write tests
+
+  // TODO: create database
   // database: client public key, pseudonyms
   // parameter: client host
+
   // TODO: verify signature
+
+  // TODO: replace platform urls to edunym urls
+
+  res.render('lti', {
+    url: req.query.client_url,
+    idToken: '',
+  });
 });
 
 module.exports = router;
